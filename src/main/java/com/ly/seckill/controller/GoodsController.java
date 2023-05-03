106da55d6a7cda7d1d3603626033db7b12aa0ffe
AstonIceDog <AstonIceDog@126.com>
2023-05-03 16:15:04 +0800

    商品列表页和商品详情页放入redis缓存。

--- src/main/java/com/ly/seckill/controller/GoodsController.java
+++ src/main/java/com/ly/seckill/controller/GoodsController.java
@@ -5,17 +5,23 @@ import com.ly.seckill.service.IGoodsService;
 import com.ly.seckill.service.IUserService;
 import com.ly.seckill.vo.GoodsVo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.thymeleaf.context.WebContext;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 /*
  * @description: some description
@@ -27,29 +33,56 @@ public class GoodsController {
     private IUserService userService;
     @Autowired
     private IGoodsService goodsService;
+    @Autowired
+    private RedisTemplate redisTemplate;
+    @Autowired
+    private ThymeleafViewResolver thymeleafViewResolver;
 
     /**
      * @description: 跳转商品列表
      * windows优化前qps1156
      * linux  优化前qps400
+     * 页面缓存
      * @param: [session, model, ticket]
      * @return: java.lang.String
      **/
-    @RequestMapping("/toList")
-    public String toList(Model model, User user){
+    @RequestMapping(value = "/toList", produces = "text/html;charset=utf-8")
+    @ResponseBody
+    public String toList(Model model, User user, HttpServletResponse response, HttpServletRequest request){
+        //Redis中获取页面，如果不为空，则直接返回页面
+        ValueOperations valueOperations = redisTemplate.opsForValue();
+        String html = (String) valueOperations.get("goodsList");
+        if (!StringUtils.isEmpty(html)){
+            return html;
+        }
         model.addAttribute("user", user);
         model.addAttribute("goodsList", goodsService.findGoodsVo());
-        return "goodsList";
+//        return "goodsList";
+        //如果为空，手动渲染，存入Redis并返回
+        WebContext context = new WebContext(request, response, request.getServletContext(), request.getLocale(), model.asMap());
+        html = thymeleafViewResolver.getTemplateEngine().process("goodsList", context);
+        if (!StringUtils.isEmpty(html)){
+            valueOperations.set("goodsList", html, 60, TimeUnit.SECONDS);
+        }
+        return null;
     }
 
 
     /**
      * @description: 跳转商品详情
+     * url缓存
      * @param: [GoodsId]
      * @return: java.lang.String
      **/
-    @RequestMapping("/toDetail/{goodsId}")
-    public String toDetail(Model model, User user, @PathVariable Long goodsId){
+    @RequestMapping(value = "/toDetail/{goodsId}", produces = "text/html;charset=utf-8")
+    @ResponseBody
+    public String toDetail(Model model, User user, @PathVariable Long goodsId, HttpServletResponse response, HttpServletRequest request){
+        ValueOperations valueOperations = redisTemplate.opsForValue();
+        //Redis中获取页面，如果不为空，直接返回页面
+        String html = (String) valueOperations.get("goodsDetail:"+goodsId);
+        if (!StringUtils.isEmpty(html)){
+            return html;
+        }
         model.addAttribute("user", user);
         GoodsVo goodsVo = goodsService.findGoodsVoByGoodsId(goodsId);
         Date startDate = goodsVo.getStartDate();
@@ -73,6 +106,12 @@ public class GoodsController {
         model.addAttribute("remainSeconds", remainSeconds);
         model.addAttribute("seckillStatus", seckillStatus);
         model.addAttribute("goods" , goodsVo);
-        return "goodsDetail";
+        WebContext context = new WebContext(request, response, request.getServletContext(), request.getLocale(), model.asMap());
+        html = thymeleafViewResolver.getTemplateEngine().process("goodsDetail", context);
+        if (!StringUtils.isEmpty(html)){
+            valueOperations.set("goodsDetail:"+goodsId, html, 60, TimeUnit.SECONDS);
+        }
+        return html;
+//        return "goodsDetail";
     }
 }
